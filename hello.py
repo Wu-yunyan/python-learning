7c3fb12dd4066fd0e319689bc5584d41d99b166e
Wu-yunyan <1431002706@qq.com>
2026-09-09 21:05:20 +0800

    D1 first input program

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("hello world")
 print('D1,2026-09-09,绿墙第一格')
+
+print('F2任务')
+name=input('请输入你的姓名：')
+age=input('请输入你的年龄：')
+print('欢迎你的到来，',age,'岁的',name)
---
+++ info.py
@@ -0,0 +1,4 @@
+#写 info.py——用 input() 让用户输入姓名和年龄，打印「你好，XX，你明年 N+1 岁」（用到变量、int()、f-string）
+name=input("请输入姓名：")
+age=input("请输入年龄：")
+print(f'你好，{name},你明年{int(age)+1}岁')
